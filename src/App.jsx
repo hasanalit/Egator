63d25c20b7d6030c4e182dc39d2ed566b14eb31f
hasanalit <tuxtayevhasan72@gmail.com>
2022-04-02 21:11:46 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Menu from './Components/Menu';
+
+function App() {
+
+
+  return (
+    <>
+    <div className='container'>
+      <Menu />
+    </div>
+    </>
+  );
+}
+
+export default App;
