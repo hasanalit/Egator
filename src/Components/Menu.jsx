3aa434f8f1395e5c5b202574f515ab2cf9721c0b
hasanalit <tuxtayevhasan72@gmail.com>
2022-04-09 23:49:59 +0500

    menu-name

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import logo from '../Images/egator.png'
 import CloseIcon from '@mui/icons-material/Close';
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import GroupIcon from '@mui/icons-material/Group';
+import SchoolIcon from '@mui/icons-material/School';
 import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
-import InsightsIcon from '@mui/icons-material/Insights';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
+// import InsightsIcon from '@mui/icons-material/Insights';
+// import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import ReceiptIcon from '@mui/icons-material/Receipt';
-import ReportGmailerrorredIcon from '@mui/icons-material/ReportGmailerrorred';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+// import ReportGmailerrorredIcon from '@mui/icons-material/ReportGmailerrorred';
 import SettingsIcon from '@mui/icons-material/Settings';
 import AddIcon from '@mui/icons-material/Add';
 import LogoutIcon from '@mui/icons-material/Logout';
@@ -27,7 +29,7 @@ function Menu({openModal, setOpenModal}) {
         <div className='top'>
           <div className='logo'>
             <img src={logo} alt="logo" />
-            <h2>EGA<span className='danger'>TOR</span></h2>
+            <h2>DAVO<span className='danger'>MAT</span></h2>
           </div>
           <div className='close' id='close-btn' onClick={() => setOpenModal(!openModal)}>
             <span className='material-icons-sharp'><CloseIcon /></span>
@@ -37,44 +39,44 @@ function Menu({openModal, setOpenModal}) {
           <div className='sidebar'>
             <a href="/" className='active'>
               <span className='material-icon-sharp'><DashboardIcon /></span>
-              <h3>Dashboard</h3>
+              <h3>Asosiy Sahifa</h3>
             </a>
             <a href="/">
-              <span className='material-icon-sharp'><PersonOutlineIcon /></span>
-              <h3>Customers</h3>
+              <span className='material-icon-sharp'><GroupIcon /></span>
+              <h3>Xodimlar davomati</h3>
             </a>
             <a href="/">
-              <span className='material-icon-sharp'><ReceiptLongIcon /></span>
-              <h3>Orders</h3>
-            </a>
-            <a href="/">
-              <span className='material-icon-sharp'><InsightsIcon /></span>
-              <h3>Anolytics</h3>
-            </a>
-            <a href="/">
-              <span className='material-icon-sharp'><MailOutlineIcon/></span>
-              <h3>Massages</h3>
-              <span className='message-count'>26</span>
+              <span className='material-icon-sharp'><SchoolIcon /></span>
+              <h3>O`quvchilar davomati</h3>
             </a>
             <a href="/">
               <span className='material-icon-sharp'><ReceiptIcon /></span>
-              <h3>Products</h3>
+              <h3>Xodimlar ro'yxati</h3>
             </a>
             <a href="/">
-              <span className='material-icon-sharp'><ReportGmailerrorredIcon /></span>
-              <h3>Reports</h3>
+              <span className='material-icon-sharp'><ReceiptLongIcon /></span>
+              <h3>O'quvchilar ro'yxati</h3>
             </a>
+            {/* <a href="/">
+              <span className='material-icon-sharp'><MailOutlineIcon/></span>
+              <h3>Massages</h3>
+              <span className='message-count'>26</span>
+            </a> */}
             <a href="/">
               <span className='material-icon-sharp'><SettingsIcon /></span>
-              <h3>Settings</h3>
+              <h3>Sozlamalar</h3>
+            </a>
+            <a href="/">
+              <span className='material-icon-sharp'><PersonAddAlt1Icon /></span>
+              <h3>Xodim qo'shish</h3>
             </a>
             <a href="/">
               <span className='material-icon-sharp'><AddIcon /></span>
-              <h3>Add Products</h3>
+              <h3>O'quvchi qo'shish</h3>
             </a>
             <a href="/">
               <span className='material-icon-sharp'><LogoutIcon /></span>
-              <h3>Logout</h3>
+              <h3>Chiqish</h3>
             </a>
 
           </div>
